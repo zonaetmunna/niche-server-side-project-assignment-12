5d79981fb18c9c93535e570beb04eb8864d60e8c
zonaetmunna <zonaetmonna@gmail.com>
2021-11-13 01:16:55 +0600

    add

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function run() {
                const result = await watchCollection.insertOne(product);
                res.json(result);
           })
-          // delete signle product
+          // delete single product
           app.delete('/products/:id', async (req, res) => {
                const id = req.params.id;
                const query = { _id: ObjectId(id) };
